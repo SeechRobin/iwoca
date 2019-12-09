3dfd4a6c594dc0bfbcab03882c7b698d0a39cf20
Robin Mukanganise <robin.mukanganise@lendinvest.com>
2019-12-09 21:28:01 +0000

    refactor

--- src/components/LoanTable/Totals.js
+++ src/components/LoanTable/Totals.js
@@ -5,15 +5,18 @@ import { rowTotal, formatCurrency } from '../../helpers/utils';
 
 const TableHeader = ({loanPayments}) => {
 
+    const principal = rowTotal('principal', loanPayments);
+    const interest = rowTotal('interest', loanPayments);
+    const totalRepayment = rowTotal('totalRepayment', loanPayments);
+
     return (
         <Table.Row>
             <Table.Cell>Total</Table.Cell>
-            <Table.Cell>{formatCurrency(rowTotal('principal', loanPayments))}</Table.Cell>
-            <Table.Cell>{formatCurrency(rowTotal('interest', loanPayments))}</Table.Cell>
-            <Table.Cell>{formatCurrency(rowTotal('totalRepayment', loanPayments))}</Table.Cell>
+            <Table.Cell>{formatCurrency(principal)}</Table.Cell>
+            <Table.Cell>{formatCurrency(interest)}</Table.Cell>
+            <Table.Cell>{formatCurrency(totalRepayment)}</Table.Cell>
         </Table.Row> 
     )
-
 
 };
     
