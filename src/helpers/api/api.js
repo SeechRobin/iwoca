ed1bbc5e4f63a1526bd5fc0dc3d89ab18d2a2c87
Robin Mukanganise <robin.mukanganise@lendinvest.com>
2019-12-09 13:31:01 +0000

    http --> https

--- src/helpers/api/api.js
+++ src/helpers/api/api.js
@@ -1,6 +1,6 @@
 import { apiWrapper } from './wrapper';
 
-const BASE_URL_API = "http://www.mocky.io/v2/5d4aa9e93300006f000f5ea9";
+const BASE_URL_API = "https://www.mocky.io/v2/5d4aa9e93300006f000f5ea9";
 
 export const getLoanRules = () => (
     apiWrapper(`${BASE_URL_API}`)
